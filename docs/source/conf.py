b74bd906eeb7717b1f34fd0fa52dca2041d17cb3
Katrin-Drys <s6kadrys@uni-bonn.de>
2024-10-20 10:26:25 +0200

    test copybotton

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -17,6 +17,10 @@ extensions = [
     'sphinx.ext.autodoc',
     'sphinx.ext.autosummary',
     'sphinx.ext.intersphinx',
+    "sphinx_panels",
+    "sphinx_copybutton",
+    "sphinx_togglebutton",
+    "sphinx_tabs.tabs",
 ]
 
 intersphinx_mapping = {
