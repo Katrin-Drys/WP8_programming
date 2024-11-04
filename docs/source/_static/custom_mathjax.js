873c595afb666a1a77702af2fbc9df3e737e8b32
Katrin-Drys <s6kadrys@uni-bonn.de>
2024-11-04 19:24:43 +0100

    .

--- docs/source/_static/custom_mathjax.js
+++ docs/source/_static/custom_mathjax.js
@@ -8,9 +8,10 @@ MathJax = {
             addLabel: [200, function (doc) {
                 for (const math of doc.math) {
                     if (math.display && math.root.tag) {
+                        // Add the label as a data attribute to the parent container
                         math.typesetRoot.parentNode.setAttribute(
                             'data-label',
-                            '\\(' + math.root.tag + '\\)'
+                            math.root.tag
                         );
                     }
                 }
@@ -18,3 +19,4 @@ MathJax = {
         }
     }
 };
+
